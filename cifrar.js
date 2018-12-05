15c496a1ce78622b8013327c3d118fc9c1a0e9a8
Msaezcardenas <msaezcardernas@gmail.com>
2018-12-04 21:24:07 -0300

    avance 2 de cifrado cesar

--- cifrar.js
+++ cifrar.js
@@ -4,22 +4,27 @@ window.cifrar = {
     let finalText = '';
     for (let i = 0; i < texto.length; i++) {
       let ascii = texto[i].charCodeAt();
+      if (ascii == 32) {
+        finalText += String.fromCharCode(ascii);
+      } else {
       if (ascii => 65 && ascii <= 90) {
         let newAscii = (ascii - 65 + offset) % 26 + 65;
         if(newAscii<65) {
           newAscii +=26;
-        } else {
-          Text = ' '
         }
         finalText += String.fromCharCode(newAscii);
       }
     }
+  }
     return finalText
   },
   decode: (texto, offset) => {
     let finalText = '';
     for (let i = 0; i < texto.length; i++) {
       let ascii = texto[i].charCodeAt();
+      if (ascii == 32) {
+        finalText += String.fromCharCode(ascii);
+      } else {
       if (ascii => 65 && ascii <= 90) {
         let newAscii = (ascii - 65 - offset) % 26 + 65;
         if (newAscii < 65) {
@@ -29,6 +34,7 @@ window.cifrar = {
       }
 
     }
+  }
     return finalText
   }
 }
